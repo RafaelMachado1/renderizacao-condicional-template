88b90bb61eab4df28b927c6acbc7bae6ed3f4d22
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-14 18:05:15 -0300

    exercicios-concluidos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styled, { createGlobalStyle } from "styled-components";
 import TelaLogin from "./components/TelaLogin/TelaLogin";
 import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
+import { useState } from "react";
+import TelaUsuarioCadastrado from "./components/TelaUsuarioCadastrado/TelaUsuarioCadastrado.js";
 
 const GlobalStyled = createGlobalStyle`
   *{
@@ -14,14 +16,67 @@ const MainContainer = styled.main`
 `
 
 function App() {
+  const [mudarTela, setMudarTela] = useState(1)
 
-  return (
-    <MainContainer >
-      <GlobalStyled />
-      <TelaLogin />
-      <TelaCadastro />
-    </MainContainer>
-  );
+  function irPara(tela) {
+    setMudarTela(tela)
+  }
+
+  //Switch case
+  switch (mudarTela) {
+    case 1:
+      return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaLogin irPara={irPara} />
+        </MainContainer>
+
+      );
+
+    case 2:
+      return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaCadastro irPara={irPara} />
+        </MainContainer>
+      );
+      
+    case 3:
+      return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaUsuarioCadastrado irPara={irPara} />
+        </MainContainer>
+      );
+  }
 }
 
 export default App;
+
+
+/*Ternário
+{mudarTela === "Login" ?
+        <TelaLogin irPara={irPara} />
+        :
+        <TelaCadastro irPara={irPara} />}*/
+
+
+
+/*If Else
+if (mudarTela === "Login") {
+    return (
+      <MainContainer >
+        <GlobalStyled />
+        <TelaLogin />
+
+      </MainContainer>
+    )
+    } else {
+      return (
+        <MainContainer >
+          <GlobalStyled />
+          <TelaCadastro />
+        </MainContainer>
+      );
+
+    }*/
